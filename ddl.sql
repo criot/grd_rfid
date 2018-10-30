38964072b425cc8be0ea38bbbac6bed0980f01f7
Navaneeth Malingan <navneetnivu07@gmail.com>
2018-10-30 16:00:31 +0530

    ddl change

--- ddl.sql
+++ ddl.sql
@@ -3,16 +3,17 @@ CREATE TABLE `grd_rfid`.`users` (
         `first_name` VARCHAR (30) NOT NULL , 
         `userid` VARCHAR (30) DEFAULT NULL ,
         `password` VARCHAR (30) DEFAULT NULL ,
-        `user_type` VARCHAR (20) NOT NULL , 
+        `user_type` VARCHAR (20) NOT NULL ,
+        `no_of_vehicles` INT DEFAULT NULL ,  
         `created_at` DATETIME NOT NULL , 
         `created_by` INT NOT NULL , 
         PRIMARY KEY(`id`),
         UNIQUE(userid)
 ) ENGINE = InnoDB;
 
-INSERT INTO users ( `first_name`,`user_type`, `created_at`, `created_by`) VALUES
-        ('Navaneeth', 'STUDENT', '2018-10-05 00:00:00', '0'),
-        ('Nivu', 'STAFF', '2018-10-05 00:00:00', '0');
+INSERT INTO users ( `first_name`,`user_type`, `no_of_vehicles`, `created_at`, `created_by`) VALUES
+        ('Navaneeth', 'STUDENT', 2, '2018-10-05 00:00:00', '0'),
+        ('Nivu', 'STAFF', 3, '2018-10-05 00:00:00', '0');
 
 INSERT INTO users ( `first_name`,`user_type`, `userid`, `password`, `created_at`, `created_by`) VALUES
         ('John', 'SECURITY', 'security1', 'security1', '2018-10-05 00:00:00', '0');
